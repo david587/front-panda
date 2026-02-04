7c61b108a0a0530ff64c7968b9e430e495f43dec
david587 <davidbarath08@gmail.com>
2026-02-04 11:23:15 +0100

    cc

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://frontpanda.com/',
-  base: '/front-panda',
   vite: {
     plugins: [tailwindcss()],
   },
